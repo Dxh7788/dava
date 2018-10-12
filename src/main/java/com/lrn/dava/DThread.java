12449422dafa2e2691feeea770cf12674f06382b
Dxh7788 <anan1215>
2018-10-12 17:51:18 +0800

    volitile 测试

---
+++ src/main/java/com/lrn/dava/DThread.java
@@ -0,0 +1,16 @@
+package com.lrn.dava;
+
+/**
+ * @author dongxiaohong on 2018/10/12 17:25
+ */
+public class DThread {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
